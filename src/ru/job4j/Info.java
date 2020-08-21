ce2fb3c0462212f6f50a138280a42f37ce5dd554
Стас <herherher233@gmail.com>
2020-08-21 21:21:45 +0300

    3.1. Загрузка файлов в github. [#243562]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -1,12 +1,8 @@
 package ru.job4j;
 
 public class Info {
-    public static class StudentInfo {
-        public static void main(String[] args) {
-            System.out.println("Stanislav Fedyanin");
-            System.out.println("09.04.1992");
-
-
-        }
+    public static void main(String[] args) {
+        System.out.println("Stanislav Fedyanin");
+        System.out.println("07.04.1992");
     }
 }

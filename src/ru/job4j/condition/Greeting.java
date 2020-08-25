31e2b3db1380d34c9004e79c88845c01482e239e
Стас <herherher233@gmail.com>
2020-08-25 20:42:45 +0300

    3.1. Загрузка файлов в github. [#243562]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!" + "But im a newbee" + " " + 2020;
+        int year = 2020;
+        System.out.println(idea);
+
+    }
+}

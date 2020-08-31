649ebff37207830c5393db756336b742e6f12e26
Стас <herherher233@gmail.com>
2020-08-31 18:18:23 +0300

    3.1. Загрузка файлов в github. [#243562]

---
+++ src/ru/job4j/condition/Cinema.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class Cinema {
+    public static void access(int age) {
+        System.out.println("The age of customer is : " + age);
+        if (age >= 21) {
+            System.out.println("Welcome to the cinema");
+        } else {
+            System.out.println("No way young pervert");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Cinema.access(21);
+        Cinema.access(16);
+
+    }
+}

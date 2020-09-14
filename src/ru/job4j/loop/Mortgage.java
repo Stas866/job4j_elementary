7c7ac95a2fabdcdaa65789e4e8f4f5c4ceb78d01
Stas866 <herherher233@gmail.com>
2020-09-14 21:44:56 +0500

    5.7. Ипотека [#346556]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(int amount, int salary, double percent) {
+        int year = 0;
+        percent = percent / 100;
+        while (amount > 0) {
+            double a = amount + (amount * percent);
+            amount = (int) (a - salary);
+            year++;
+        }
+        return year;
+    }
+}

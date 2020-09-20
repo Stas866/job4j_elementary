b8503ae55c2e6b91e3017d26d72c875420ab4811
Stas866 <herherher233@gmail.com>
2020-09-20 16:59:07 +0500

    6.4. Слово начинается с ... [#346469]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -6,6 +6,7 @@ public class ArrayChar {
         for (char index = 0; index < word.length && index < pref.length; index++) {
             if (word[index] != pref[index]) {
                 result = false;
+                break;
             }
 
         }
